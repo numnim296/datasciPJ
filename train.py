69458ab3f6c78006e0a6abbf327f14aa3e64c316
numnim296 <muthita.mu.61@ubu.ac.th>
2020-03-09 22:52:47 +0700

    final

---
+++ train.py
@@ -0,0 +1,22 @@
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.naive_bayes import MultinomialNB
+from sklearn.pipeline import make_pipeline
+from sklearn.datasets import fetch_20newsgroups
+import numpy as np
+
+categories = ['rec.motorcycles','rec.sport.baseball','sci.electronics']
+
+train = fetch_20newsgroups(subset='train', categories=categories)
+
+model = make_pipeline(TfidfVectorizer(), MultinomialNB())
+
+model.fit(train.data, train.target)
+
+from joblib import dump
+dump(model, './myapp/static/chatgroup.model')
+dump(train, './myapp/static/train.model')
+
+print(round((model.score(train.data, train.target)*100),2),'%')
+
+
+
